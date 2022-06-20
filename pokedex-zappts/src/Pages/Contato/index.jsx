fe52a87b3ab94b1ac289e197efba85eb86005314
Dienay Lima <dienaylima@gmail.com>
2022-06-20 16:55:29 -0300

    Adiciona página de contato

---
+++ pokedex-zappts/src/Pages/Contato/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../../Components/Header';
+
+import './style.scss';
+
+function Contato() {
+  return (
+    <section className="contato">
+      <Header />
+      <div>Contato</div>
+    </section>
+  );
+}
+
+export default Contato;
