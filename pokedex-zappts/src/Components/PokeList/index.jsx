5ec9b89676c5913546e74e18a970c6da5766d021
Dienay Lima <dienaylima@gmail.com>
2022-06-21 17:21:22 -0300

    Otimiza paginação da lista de pokemons

--- pokedex-zappts/src/Components/PokeList/index.jsx
+++ pokedex-zappts/src/Components/PokeList/index.jsx
@@ -10,64 +10,78 @@ import DetailPokemon from '../DetailPokemon';
 const baseUrl = "https://pokeapi.co/api/v2/pokemon/";
 
 function Pokelist() {
-    const [pokelist, setPokelist] = useState([]);
-    const [text, setText] = useState('');
-    const [typeFilter, setTypeFilter] = useState([])
+    // Quantidade de Pokemons por página
+    const limit = 24;
 
-    // Pagination
-    const [currentUrl, setCurrentUrl] = useState(`${baseUrl}?limit=24`);
-    const [nextUrl, setNextUrl] = useState('');
-    const [prevUrl, setPrevUrl] = useState('');
-    const [currentPage, setCurrenPage] = useState(1);
+    // Lista de Pokemons
+    const [pokelist, setPokelist] = useState([]);
+
+    // Pesquisa Pokemon
+    const [text, setText] = useState('');
+
+    // Usado par pegar detalhes do Pokemon
     const [showDetail, setShowDetail] = useState('');
     const [pokeName, setPokeName] = useState('');
 
+    // Variáveis para lidar com paginação
+    const [currentPage, setCurrenPage] = useState(1);
+    const [offset, setOffset] = useState(0);
+
+    // Abre card com detalhes do Pokemon
     const openDetailChange = (pokeName) => {
         setPokeName(pokeName)
         setShowDetail('show')
-        console.log("abre")
-        console.log(pokeName)
-        console.log(showDetail)
     }
 
+    // Fecha card com detalhes do Pokemon
     const closeDetailChange = () => {
         setPokeName('')
         setShowDetail('')
-        console.log(showDetail)
     }
 
+    // Carrega Lista de Pokemons
     useEffect(() => {
         axios
-            .get(currentUrl)
+            .get(`${baseUrl}?offset=${offset}&limit=${limit}`)
             .then(response => {
                 setPokelist(response.data.results);
-                console.log(response.data.results);
-                setNextUrl(response.data.next)
-                setPrevUrl(response.data.previous)
             })
             .catch(err => {
                 console.log(err);
             })
-    }, [currentUrl]);
+    }, [currentPage, offset]);
 
+    // Vai para a página seguinte
     function gotoNextPage() {
         if (currentPage < 7) {
-            setCurrentUrl(nextUrl)
+            setOffset(offset + limit)
             setCurrenPage(currentPage + 1)
         }
     }
 
+
+    // Vai para a página anterior
     function gotoPrevPage() {
         if (currentPage > 1) {
-            setCurrentUrl(prevUrl)
+            setOffset(offset - limit)
             setCurrenPage(currentPage - 1)
         }
     }
 
+    // Lida com lista para paginação
     useEffect(() => {
-        const pageList = document.querySelector('.page').children;
+        // Carrega lista numerade de páginas
+        const pageList = document.querySelector('.page');
 
-        for (let page of pageList) {
+        // Aguarda elemento ser clicado para carregar página correspondente
+        pageList.addEventListener('click', (e) => {
+            const currentPage = parseInt(e.target.innerText)
+            setCurrenPage(currentPage)
+            setOffset((currentPage - 1) * limit)
+        })
+
+        //Indica na lista a página atual da lista
+        for (let page of pageList.children) {
             const pageCurrent = parseInt(page.innerText)
 
             if (pageCurrent === currentPage) {
@@ -78,7 +92,9 @@ function Pokelist() {
         }
     }, [currentPage])
 
+    // Lida com pesquisa de Pokemons
     useEffect(() => {
+        // Procura pokemon através do nome digitado no input
         if (text) {
             fetch(`${baseUrl}${text}`)
                 .then((response) => response.json())
@@ -86,17 +102,18 @@ function Pokelist() {
                     setPokelist([response])
                 })
         }
-        if(!text) {
+        // Responsável por mostrar lista de pokemons apoś o nonme ser apagado do input
+        if (!text) {
             axios
-            .get(currentUrl)
-            .then(response => {
-                setPokelist(response.data.results);
-            })
-            .catch(err => {
-                console.log(err);
-            })
+                .get(`${baseUrl}?offset=${offset}&limit=${limit}`)
+                .then(response => {
+                    setPokelist(response.data.results);
+                })
+                .catch(err => {
+                    console.log(err);
+                })
         }
-    }, [text])
+    }, [text, offset])
 
     return (
         <section>
